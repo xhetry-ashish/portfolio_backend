0faba7bdbfff8e4d6ea9c500f9cc7c2510f52631
xhetry-ashish <ashishxhetry2017@gmail.com>
2022-04-20 10:47:46 +0545

    userProfile controller try catch added

--- src/controllers/userProfile.js
+++ src/controllers/userProfile.js
@@ -3,24 +3,31 @@ import Profile from "../models/profile.js";
 
 //getting a profile using id
 export const getSingleProfile = async (req, res) => {
-  if (!mongoose.isValidObjectId(req.params.id)) {
-    return res.status(400).send("Invalid Profile Id..");
+  try {
+    if (!mongoose.isValidObjectId(req.params.id)) {
+      throw "Invalid Profile Id";
+    }
+    const profile = await Profile.findById(req.params.id);
+    if (!profile) {
+      throw "Profile Id Not found";
+    }
+    res.status(200).json({ success: true, data: profile });
+  } catch (err) {
+    res.json({ success: false, message: err });
   }
-
-  const profile = await Profile.findById(req.params.id);
-  if (!profile) {
-    res.status(500).json({ success: false });
-  }
-  res.send(profile);
 };
 
 //get all profiles
 export const getAllProfiles = async (req, res) => {
-  const profile = await Profile.find();
-  if (!profile) {
-    res.status(500).json({ success: false });
+  try {
+    const profile = await Profile.find();
+    if (!profile) {
+      throw "No Profiles Found";
+    }
+    res.status(200).json({ success: true, data: profile });
+  } catch (err) {
+    res.json({ success: false, message: err });
   }
-  res.send(profile);
 };
 
 //creating profile
@@ -34,56 +41,64 @@ export const addProfile = async (req, res) => {
     githubLink: req.body.githubLink,
     linkedinLink: req.body.linkedinLink,
   });
+  try {
+    profiledata = await profiledata.save();
+    if (!profiledata) {
+      throw err;
+    }
 
-  profiledata = await profiledata.save();
-  if (!profiledata) {
-    res.status(500).json({
-      success: false,
-      message: "Profile cannot be created..",
-    });
+    res.status(200).json({ success: true, data: profiledata });
+  } catch (err) {
+    res.json({ success: false, message: err.message });
   }
-
-  res.status(200).send(profiledata);
 };
 
 //editing profile
 export const editProfile = async (req, res) => {
-  if (!mongoose.isValidObjectId(req.params.id)) {
-    return res.status(400).send("Invalid Profile Id..");
+  let profiledata = {
+    name: req.body.name,
+    profileRole: req.body.profileRole,
+    profileTagline: req.body.profileTagline,
+    facebookLink: req.body.facebookLink,
+    instagramLink: req.body.instagramLink,
+    githubLink: req.body.githubLink,
+    linkedinLink: req.body.linkedinLink,
+  };
+
+  try {
+    if (!mongoose.isValidObjectId(req.params.id)) {
+      throw "Invalid Profile Id";
+    }
+
+    let data = await Profile.findByIdAndUpdate(req.params.id, profiledata, {
+      new: true,
+    });
+
+    if (!data) {
+      throw "The profile cannot be updated..";
+    }
+    res.status(200).json({ success: true, data: data });
+  } catch (err) {
+    res.json({ success: false, message: err.message });
   }
-
-  let profiledata = await Profile.findByIdAndUpdate(
-    req.params.id,
-    {
-      name: req.body.name,
-      profileRole: req.body.profileRole,
-      profileTagline: req.body.profileTagline,
-      facebookLink: req.body.facebookLink,
-      instagramLink: req.body.instagramLink,
-      githubLink: req.body.githubLink,
-      linkedinLink: req.body.linkedinLink,
-    },
-    { new: true }
-  );
-
-  if (!profiledata)
-    return res.status(400).send("The profile cannot be updated..");
-
-  res.status(200).send(profiledata);
 };
 
 //deleting profile
 export const deleteProfile = async (req, res) => {
-  if (!mongoose.isValidObjectId(req.params.id)) {
-    return res.status(400).send("Invalid Profile Id..");
-  }
+  try {
+    if (!mongoose.isValidObjectId(req.params.id)) {
+      throw "Invalid Profile Id";
+    }
 
-  const profile = await Profile.findByIdAndRemove(req.params.id);
-  if (!profile) {
-    res.status(500).json({ success: false });
+    const profile = await Profile.findByIdAndRemove(req.params.id);
+    if (!profile) {
+      throw "Profile Not Found";
+    }
+    return res.status(200).json({
+      success: true,
+      message: " profile is deleted",
+    });
+  } catch (err) {
+    res.json({ success: false, message: err });
   }
-  return res.status(200).json({
-    success: true,
-    message: " profile is deleted..",
-  });
 };
