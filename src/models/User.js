0974687f33345719117959db8b5832709644b84a
xhetry-ashish <ashishxhetry2017@gmail.com>
2022-04-20 11:10:57 +0545

    user register model added

---
+++ src/models/User.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+
+    email: {
+      type: string,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+    },
+
+    contact: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//making  model
+const User = mongoose.model("user", userSchema);
+export default User;
